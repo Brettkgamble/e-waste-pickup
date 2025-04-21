b8e699156d93bae000f2b21610ff93a0a50f60ef
Brett Gamble <brettkgamble@gmail.com>
2025-04-21 15:07:18 -0600

    0007 - minor change to email template for resend

--- apps/web/src/app/api/send/route.ts
+++ apps/web/src/app/api/send/route.ts
@@ -1,18 +1,18 @@
+/* eslint-disable prettier/prettier */
 import type * as React from "react";
 import { Resend } from "resend";
 
 import { EmailTemplate } from "../../../components/email-template";
 
-// eslint-disable-next-line turbo/no-undeclared-env-vars
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: "Brett <admin@fine-metals-recovery.ca",
-      to: ["brettkgamble@gmail.com"],
+      from: "admin <admin@e-waste-pickup.ca>",
+      to: ["admin@e-waste-pickup.ca"],
       subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+      react: EmailTemplate({ firstName: "Brett" }) as React.ReactElement,
     });
 
     if (error) {
