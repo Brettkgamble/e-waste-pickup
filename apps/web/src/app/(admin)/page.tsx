c3c57a77613fc9dabbf1a1b2ae862d54a05e3ba4
Brett Gamble <brettkgamble@gmail.com>
2025-06-01 09:32:37 -0600

    0015b - /admin/page build issues on vercel

--- apps/web/src/app/(admin)/page.tsx
+++ apps/web/src/app/(admin)/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import { auth } from "@/auth";
 
 export default async function Page() {
-  const session = await auth();
+  // const session = await auth();
 
   return (
     <div>
       <h1>NextAuth V5 and Next.js 15</h1>
-      <p>User sign in with name {session?.user?.name}</p>
+      {/* <p>User sign in with name {session?.user?.name}</p>
       <p>User sign in with email {session?.user?.email}</p>
       {session?.user?.image && (
         <Image
@@ -18,7 +18,7 @@ export default async function Page() {
           alt={session.user.name ?? "Avatar"}
           className="rounded-full"
         />
-      )}
+      )} */}
     </div>
   );
 }
