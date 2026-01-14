4be76150aed5ed4e7b69969a74725f3d6f36117d
Brett Gamble <brettkgamble@gmail.com>
2026-01-14 14:10:45 -0700

    0095 - Ran codemod for middleware to proxy

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import { auth } from "@/auth";
 
 const protectedRoutes = ["/admin"];
-export default async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const session = await auth();
 
   const { pathname } = request.nextUrl;
